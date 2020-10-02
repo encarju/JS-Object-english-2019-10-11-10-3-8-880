eb42d7f4df9829e18ad1206992f5858cf4f047e6
encarju <justine.encarnacion@oocl.com>
2020-10-02 11:25:37 +0800

    Updated for Answers

---
+++ main.js
@@ -0,0 +1,18 @@
+//PROGRAM 1
+var user = {};
+user.name = "John";
+user.surname = "Mike";
+user.name = "Peter";
+delete user.name;
+
+
+
+//PROGRAM2
+var fruit = {
+    apple: 20,
+    pear: 20,
+    peach: 10
+};
+var pieces;
+pieces = fruit.apple+fruit.pear+fruit.peach;
+console.log(pieces);
